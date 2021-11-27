07ef669e9dcf563948f6df8678c89a568d61b682
장서윤 <may-primavera@naver.com>
2021-11-27 21:09:46 +0900

    Practice05 과제

---
+++ src/com/javaex/ex09/StringUtil.java
@@ -0,0 +1,16 @@
+package com.javaex.ex09;
+
+public class StringUtil {
+	//필드
+	
+	//생성자
+	
+	//메소드 g,s
+	
+	//메소드 일반
+    public static String concatString(String[] strArray){
+    	
+    	return "SuperMan"+"BatMan"+"SpiderMan";
+    }
+
+}
