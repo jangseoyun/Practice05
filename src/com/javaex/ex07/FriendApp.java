f192f93ea59367065837a5d368c07c6f25eb8b70
장서윤 <may-primavera@naver.com>
2021-11-28 19:52:25 +0900

    Practice05 1~7

--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -21,21 +21,21 @@ public class FriendApp {
 		friendArray[2] = friend03;
 
         //친구정보 3명 입력 로직 --> 반복문 사용
-            for(int i = 0; i<friendArray.length; i++ ) {
-            	System.out.print("이름: ");
-            	String name = sc.next();
-            	friendArray[i].setName(name);
-            	
-            	System.out.print("핸드폰: ");
-            	String hp = sc.next();
-            	friendArray[i].setHp(hp);
-            	
-            	System.out.print("학교: ");
-            	String school = sc.next();
-            	friendArray[i].setSchool(school);
+        for(int i = 0; i<friendArray.length; i++ ) {
+        	System.out.print("이름: ");
+        	String name = sc.next();
+        	friendArray[i].setName(name);
+        	
+        	System.out.print("핸드폰: ");
+        	String hp = sc.next();
+        	friendArray[i].setHp(hp);
+        	
+        	System.out.print("학교: ");
+        	String school = sc.next();
+        	friendArray[i].setSchool(school);
 
-            	System.out.println("----------------------------");
-            }
+        	System.out.println("----------------------------");
+        }
         
         // 친구정보 출력
         for (int i = 0; i < friendArray.length; i++) {
