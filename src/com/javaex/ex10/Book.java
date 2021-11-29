c74e09f5ed9d7be250934bae22b8d30d6c4b7ddc
장서윤 <may-primavera@naver.com>
2021-11-29 17:28:07 +0900

    Practice05 과제

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -1,12 +1,12 @@
 package com.javaex.ex10;
-// 못 풀었음..
+
 public class Book {
 
 	// 필드
 	private int bookNo;
 	private String title;
 	private String author;
-	private int stateCode;
+	private int stateCode =1;
 
 	// 생성자
 	public Book() {
@@ -16,10 +16,6 @@ public class Book {
 		this.bookNo = bookNo;
 		this.title = title;
 		this.author = author;
-	}
-
-	public Book(int stateCode) {
-		this.stateCode = 1;
 	}
 
 	// 메소드 g,s
@@ -48,25 +44,27 @@ public class Book {
 	}
 	
 	//메소드 일반
+	//대여기능 수행 
 	
 	public void rent() {
-		
+		System.out.println(title+"이(가) 대여됐습니다.");	
+		stateCode=0;
 	}
 	
 	public void print(String[] print) {
-		System.out.println(bookNo+"책 제목: "+title+", 작가: "+author+", 대여 유무: "+stateCode);
+		String rentCode;
+		if(stateCode==0) {
+			rentCode = "대여중";
+		}else{
+			rentCode = "재고있음";
+		}
+		
+		System.out.println(bookNo+"책 제목: "+title+", 작가: "+author+", 대여 유무: "+rentCode);
 	}
 
 	@Override
 	public String toString() {
 		return "Book [bookNo=" + bookNo + ", title=" + title + ", author=" + author + ", stateCode=" + stateCode + "]";
 	}
-
-	
-	
-	
-	
-		
-	
 
 }
